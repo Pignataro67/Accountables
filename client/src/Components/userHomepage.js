c3baa20db2d88f795e239e2cfe6d21db80edf63e
Pignataro67 <Pignataro67@gmail.com>
2020-10-20 05:02:40 -0700

    adding comp.

---
+++ client/src/Components/userHomepage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+class UserHomepage extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      allSessions: []
+    }
+  }
+
+  render() {
+    return (
+      <h1> All Day </h1>
+    )
+  }
+}
+
+export default UserHomepage;
