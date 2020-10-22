cda7a0461ef359db974c6fecaa5ac4c38138f87f
Pignataro67 <Pignataro67@gmail.com>
2020-10-22 07:34:56 -0700

    mo skeleton comps.

---
+++ client/src/login.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Login extends Component {
+
+  render() {
+    return (
+    <h3>Hi</h3>
+    );
+  }
+}
+
+export default Login;
---
+++ client/src/sandbox.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import ReactCountdownClock from "react-countdown-clock";
+
+class Timer extends Component {
+  myCallback = () => {
+    return "Done";
+  };
+
+
+  render() {
+    
+    };
+
+    return (
+      <ReactCountdownClock/>
+    );
+  }
+}
+
+export default Timer;
