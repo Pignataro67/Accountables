b375493599f91074428eb31b2dd510d4ec73ee6f
Pignataro67 <Pignataro67@gmail.com>
2020-10-22 07:31:43 -0700

    adding skeletons

---
+++ client/src/Components/workSessionComponents/note.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+note = () =>  {
+  
+  return (
+    <h2>Hello</h2>
+  )
+}
+
+
+export default note;
---
+++ client/src/Components/workSessionComponents/task.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+task = () => {
+
+  return (
+    <div> What Up</div>
+  )  
+};
+
+export default task;
