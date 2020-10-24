31ecbe87a083826bb4d3f1d9704c16cddbdc0dc7
Pignataro67 <Pignataro67@gmail.com>
2020-10-24 11:28:19 -0700

    adding comp.

---
+++ client/src/Components/taskList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class TaskList extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      allSessions: []
+    }
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default TaskList;
