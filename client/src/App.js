e775bf5cc83c1a1697b9dcba3c33b84d5504faca
Pignataro67 <Pignataro67@gmail.com>
2020-10-26 04:45:30 -0700

    refractoring

--- client/src/App.js
+++ client/src/App.js
@@ -35,32 +35,11 @@ class App extends Component {
         }, ()=> {getCurrentSession()})
       }
   
-      if (this.state.currentSession.length === 0) {
-        const lastSession = this.state.workSessions[this.state.workSessions.length - 1]
-        if (lastSession.start_time === ""){
-          const sessions = this.state.workSessions.filter(session => session !== lastSession)
-          this.setState({
-            ...this.state,
-            currentSession: lastSession,
-            workSessions: sessions
-          })
-        } else {
-          fetch("http://localhost:3001/work_sessions", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              "Accept": "application/json"
-            },
-            body: JSON.stringify({
-              start_time: "",
-              end_time: "",
-              total_time: 20,
-              note: "",
-              user_id: this.state.user.id
-          })
-        })
-        .then(resp => resp.json())
-        .then(wsData => {
+      const getCurrentSession = () => {
+        if (this.state.currentSession.length === 0) {
+          const lastSession = this.state.workSessions[this.state.workSessions.length - 1]
+          if (lastSession.start_time === ""){
+            const sessions = this.state.workSessions.filter(session => session !== lastSession)  
           this.setState({
             ...this.state,
             currentSession: wsData
