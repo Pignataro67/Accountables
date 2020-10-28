b52611bb12b21a6b22936f3cda9d51aa31c556da
Pignataro67 <Pignataro67@gmail.com>
2020-10-28 05:32:59 -0700

    syntax debug

--- client/src/Components/timer.js
+++ client/src/Components/timer.js
@@ -25,27 +25,27 @@ import ReactCountdownClock from "react-countdown-clock";
       const minutes = 20;
 
       return (
+        <div className="timer">
         <ReactCountdownClock
         seconds={60 * minutes}
           color="#232323"
           alpha={0.5}
           size={150}
           onComplete={this.myCallback}
-          paused= {this.state.pausation}
+          paused = {this.state.pausation}
           weight={20}
-          onClick={()=> toggle(pausation)}
         />
           <div>
+            </div>
+              <div className="timer-btn">
+              {this.state.pausation ? (
+              <button className="start-button" onClick={() =>this.toggle()} >Start</button>):
 
+              (<button onClick={()=> this.toggle()} 
+              className="pause-button">Pause</button>)
+            }
+            </div>
           </div>
-            <div className="timer-btn">
-            {this.state.pausation ? (
-            <button className="start-button" onClick={() =>this.toggle()} >Start</button>):
-
-            (<button onClick={()=> this.toggle()} 
-            className="pause-button">Pause</button>)
-          }
-        </div>
       );
     }
   }
