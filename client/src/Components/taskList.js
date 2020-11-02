b93e2d634875045fd69580d220d9a6a4bb24a5a7
Pignataro67 <Pignataro67@gmail.com>
2020-11-02 05:45:06 -0800

    adding another return

--- client/src/Components/taskList.js
+++ client/src/Components/taskList.js
@@ -4,9 +4,27 @@ const TaskList  = props => {
 
   const displayTasks = () => {
     return props.tasks.map((task, idx) => {
-      return <li key={idx}>{task.title}<button className="delete-button" name="delete-button" onClick={()=> props.deleteTask(task)}>X</button>
-      <button className="edit-button" name="edit-button">E</button></li>
+      return props.working ?
+      createCheckedTasks(task, idx) : createEditTasks(task, idx)
     });
+
+  }
+
+  const createEditTasks = (task, idx) => {
+    return <li key={idx}>
+    <input type="checkbox" name={task.title} disabled />
+    {task.title}
+    <button className="delete-button" name="delete-button" onClick={()=> props.deleteTask(task)}>X</button>
+    <button className="edit-button" name="edit-button">E</button>
+    </li>
+  }
+
+  const createCheckedTasks = (task, idx) => {
+    return <li key={idx}>
+    <input type="checkbox" name={task} />
+    {task.title}
+
+    </li>
   }
 
     return (
@@ -15,7 +33,7 @@ const TaskList  = props => {
         <input type="text" name="taskInput" />
         <input type="submit" value="Add"/>
       </form>
-      <ul>
+      <ul  className="task-list">
         {displayTasks()}
       </ul>
       </div>
